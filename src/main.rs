bd5c1556da66cd4f5e4be94a0ab152a45fb1bf75
Adham Salama <adham.salama@wuilt.com>
2023-02-03 21:29:02 +0200

    Update version

--- src/main.rs
+++ src/main.rs
@@ -3,29 +3,12 @@ mod http;
 mod server;
 use http::Request;
 use server::Server;
-use std::net::TcpListener;
 
 fn main() {
-    // let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
-    // let pool = ThreadPool::new(4);
-    // let server = Server {
-    //     threadpool: pool,
-    //     job: Box::new(userFn),
-    //     listener,
-    // };
-    let server = Server::new("127.0.0.1:7878", Box::new(userFn));
+    let server = Server::new("127.0.0.1:7878", Box::new(user_fn));
     server.start();
-    // for stream in listener.incoming() {
-    //     let stream = stream.unwrap();
-    //     pool.execute(|| {
-    //         let (mut stream, request) = handle_connection(stream);
-    //         print_http_request(request);
-    //         let response = "HTTP/1.1 200 OK\r\n\r\n".to_owned();
-    //         stream.write_all(response.as_bytes()).unwrap();
-    //     });
-    // }
 }
 
-fn userFn(req: &Request) {
+fn user_fn(req: &Request) {
     println!("My Handler!!!");
 }
