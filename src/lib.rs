c5cfc9686e5bd9bb8d95b2bcdb79c0ce4b3ae9cd
Adham Salama <adham.salama@wuilt.com>
2023-02-10 23:42:55 +0200

    Add crate doc strings

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,12 @@
+//! # Blitzkrieg
+//!
+//! An HTTP Web Server written from scratch in Rust.
+//!
+//! This is written for educational purposes and is not meant to be used in production.
+
+/// A module for parsing HTTP.
 pub mod http;
+/// A module for implementing a Server struct.
 pub mod server;
+/// A module for implementing a threadpool for the server.
 pub mod threadpool;
