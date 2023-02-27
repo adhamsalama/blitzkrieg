945a1cc180ec6e395b7425e440c23a547b71ae17
Adham Salama <adham.salama@wuilt.com>
2023-02-27 22:59:04 +0200

    Add package logo

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@
 //! An HTTP Web Server written from scratch in Rust.
 //!
 //! This is written for educational purposes and is not meant to be used in production.
-
+#![doc(html_logo_url = "https://cdn-icons-png.flaticon.com/512/3857/3857541.png")]
 /// A module for parsing HTTP.
 pub mod http;
 /// A module for implementing a Server struct.
