9037b28856d1cc6d14a92013f1eda339b912a7d3
Adham Salama <adham.salama@wuilt.com>
2023-04-21 18:42:25 +0200

    Update tests

--- src/test.rs
+++ src/test.rs
@@ -51,19 +51,20 @@ pub mod tests {
     fn parse_http_string_works() {
         let req = Request::parse("GET /get-request-test HTTP/1.1\r\nHost: localhost:7878\r\nAccept-Encoding: gzip, deflate, br\r\nAccept: */*\r\nConnection: keep-alive".into(),
             vec![],
-        );
+        ).unwrap();
         assert_eq!(req.method, HTTPMethod::GET);
         assert_eq!(req.path, "/get-request-test");
         assert_eq!(req.headers.get("Host").unwrap(), "localhost:7878");
     }
     #[test]
     fn parse_http_string_plain_body_works() {
-        let req = Request::parse("POST / HTTP/1.1\r\nHost: localhost:7878\r\nUser-Agent: insomnia/2022.7.3\r\nContent-Type: text/plain\r\nAuthorization: token\r\nAccept: */*\r\nContent-Length: 15\r\n\r\n".into(), vec![115, 111, 109, 101, 32, 112, 108, 97, 105, 110, 32, 98, 111, 100, 121]);
+        let req = Request::parse("POST / HTTP/1.1\r\nHost: localhost:7878\r\nUser-Agent: insomnia/2022.7.3\r\nContent-Type: text/plain\r\nAuthorization: token\r\nAccept: */*\r\nContent-Length: 15\r\n\r\n".into(), vec![115, 111, 109, 101, 32, 112, 108, 97, 105, 110, 32, 98, 111, 100, 121]).unwrap();
         assert_eq!(req.method, HTTPMethod::POST);
         assert_eq!(req.headers.get("Content-Type").unwrap(), "text/plain");
         match req.body.unwrap() {
             http::BodyType::Text(body) => assert_eq!(body, "some plain body"),
             http::BodyType::FormdataBody(_) => panic!("Bodytype shouldn't be formdata"),
+            http::BodyType::File(_) => panic!("Bodytype shouldn't be file"),
         }
     }
     #[test]
@@ -74,7 +75,7 @@ pub mod tests {
         let r = "POST / HTTP/1.1\r\nHost: localhost:7878\r\nUser-Agent: insomnia/2022.7.3\r\nContent-Type: text/plain\r\nAuthorization: token\r\nAccept: */*\r\nContent-Length: 15\r\n\r\n";
         let v = [r.into(), body].concat();
         s.set_read_buffer(v.into());
-        let req = Request::from_tcp_stream(&mut s);
+        let req = Request::from_tcp_stream(&mut s).unwrap();
         match req.body {
             Some(http::BodyType::Text(body)) => {
                 assert_eq!(body, body_string)
@@ -84,7 +85,7 @@ pub mod tests {
     }
     #[test]
     fn test_empty_body() {
-        let req = Request::parse("POST / HTTP/1.1\r\nHost: localhost:7878\r\nUser-Agent: insomnia/2022.7.3\r\nContent-Type: text/plain\r\nAuthorization: token\r\nAccept: */*\r\nContent-Length: 15\r\n\r\n".into(), vec![]);
+        let req = Request::parse("POST / HTTP/1.1\r\nHost: localhost:7878\r\nUser-Agent: insomnia/2022.7.3\r\nContent-Type: text/plain\r\nAuthorization: token\r\nAccept: */*\r\nContent-Length: 15\r\n\r\n".into(), vec![]).unwrap();
         match req.body {
             None => assert!(true),
             _ => panic!("Body should be None"),
@@ -93,7 +94,7 @@ pub mod tests {
     #[test]
     fn parse_http_string_formdata_body_works() {
         let req = Request::parse("POST / HTTP/1.1\r\nHost: localhost:7878\r\nUser-Agent: insomnia/2022.7.3\r\nContent-Type: multipart/form-data; boundary=X-INSOMNIA-BOUNDARY\r\nAuthorization: dqweqw\r\nAccept: */*\r\nContent-Length: 175\r\n\r\n".into(), vec![45, 45, 88, 45, 73, 78, 83, 79, 77, 78, 73, 65, 45, 66, 79, 85, 78, 68, 65, 82, 89, 13, 10, 67, 111, 110, 116, 101, 110, 116, 45, 68, 105, 115, 112, 111, 115, 105, 116, 105, 111, 110, 58, 32, 102, 111, 114, 109, 45, 100, 97, 116, 97, 59, 32, 110, 97, 109, 101, 61, 34, 110, 97, 109, 101, 34, 13, 10, 13, 10, 97, 100, 104, 111, 109, 13, 10, 45, 45, 88, 45, 73, 78, 83, 79, 77, 78, 73, 65, 45, 66, 79, 85, 78, 68, 65, 82, 89, 13, 10, 67, 111, 110, 116, 101, 110, 116, 45, 68, 105, 115, 112, 111, 115, 105, 116, 105, 111, 110, 58, 32, 102, 111, 114, 109, 45, 100, 97, 116, 97, 59, 32, 110, 97, 109, 101, 61, 34, 97, 103, 101, 34, 13, 10, 13, 10, 50, 51, 13, 10, 45, 45, 88, 45, 73, 78, 83, 79, 77, 78, 73, 65, 45, 66, 79, 85, 78, 68, 65, 82, 89, 45, 45, 13, 10]
-    );
+    ).unwrap();
         assert_eq!(req.method, HTTPMethod::POST);
         assert_eq!(
             req.headers.get("Content-Type").unwrap(),
@@ -101,6 +102,7 @@ pub mod tests {
         );
         match req.body.unwrap() {
             http::BodyType::Text(_) => panic!("Bodytype shouldn't be text/plain"),
+            http::BodyType::File(_) => panic!("Bodytype shouldn't be file"),
             http::BodyType::FormdataBody(body) => {
                 let mut expected_body: Vec<FormdataText> = Vec::new();
                 expected_body.push(FormdataText {
