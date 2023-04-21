95f752345d9a086b18d585e6281722686f124e30
Adham Salama <adham.salama@wuilt.com>
2023-04-21 16:35:00 +0200

    Refactor HTTP to its own module

--- src/http/parser.rs
+++ src/http/parser.rs
@@ -1,76 +1,9 @@
+use super::{BodyType, FormdataBody, FormdataFile, FormdataText, HTTPMethod, Request};
 use std::{
     collections::HashMap,
     io::{BufRead, BufReader, Read, Write},
     str::FromStr,
 };
-
-#[derive(Debug, PartialEq)]
-pub enum HTTPMethod {
-    GET,
-    POST,
-    PUT,
-    PATCH,
-    DELETE,
-    HEAD,
-    OPTIONS,
-}
-impl FromStr for HTTPMethod {
-    type Err = ();
-    /// Creates an HTTP method enum value from a str.
-    fn from_str(input: &str) -> Result<HTTPMethod, Self::Err> {
-        match input {
-            "GET" => Ok(HTTPMethod::GET),
-            "POST" => Ok(HTTPMethod::POST),
-            "PUT" => Ok(HTTPMethod::PUT),
-            "PATCH" => Ok(HTTPMethod::PATCH),
-            "DELETE" => Ok(HTTPMethod::DELETE),
-            "HEAD" => Ok(HTTPMethod::HEAD),
-            "OPTIONS" => Ok(HTTPMethod::OPTIONS),
-            _ => Err(()),
-        }
-    }
-}
-
-#[derive(Debug, PartialEq)]
-pub struct FormdataText {
-    pub name: String,
-    pub value: String,
-}
-
-#[derive(Debug, PartialEq)]
-pub struct FormdataFile {
-    pub name: String,
-    pub file_name: String,
-    pub content: Vec<u8>,
-}
-pub enum Formdata {
-    FormdataText(FormdataText),
-    FormdataFile(FormdataFile),
-}
-
-#[derive(Debug, PartialEq)]
-/// Formdata body struct.
-pub struct FormdataBody {
-    /// Formdata text fields.
-    pub fields: Option<Vec<FormdataText>>,
-    /// Formdata files.
-    pub files: Option<Vec<FormdataFile>>,
-}
-
-#[derive(Debug)]
-/// HTTP Request body type.
-pub enum BodyType {
-    Text(String),
-    FormdataBody(FormdataBody),
-}
-#[derive(Debug)]
-/// HTTP Request struct.
-pub struct Request {
-    pub method: HTTPMethod,
-    pub path: String,
-    pub headers: HashMap<String, String>,
-    pub body: Option<BodyType>,
-}
 
 impl Request {
     /// Constructs an HTTP Request from a TCP Stream.
@@ -230,52 +163,5 @@ impl Request {
             fields: form_fields,
             files: form_files,
         }
-    }
-}
-
-/// HTTP Response struct.
-pub struct Response {
-    pub status_code: u16,
-    pub headers: Option<HashMap<String, String>>,
-    pub body: Option<Vec<u8>>,
-}
-
-impl Response {
-    /// Creates an HTTP Response.
-    pub fn new(status_code: u16) -> Self {
-        Self {
-            status_code,
-            headers: None,
-            body: None,
-        }
-    }
-
-    /// Set reponse headers.
-    pub fn headers(mut self, headers: HashMap<String, String>) -> Self {
-        self.headers = Some(headers);
-        self
-    }
-
-    /// Set reponse body.
-    pub fn body(mut self, body: &str) -> Self {
-        self.body = Some(body.as_bytes().to_vec());
-        self
-    }
-
-    /// Turns an HTTP Response into bytes.
-    pub fn into_bytes(self) -> Vec<u8> {
-        let mut res = String::from("HTTP/1.1 ");
-        let status_code = self.status_code.to_string();
-        res = format!("{}{}", res, status_code);
-        res.push_str("\r\n");
-        let q = self.headers.unwrap_or_default();
-        for (key, value) in q {
-            res = format!("{}{}: {}\r\n", res, key, value);
-        }
-        res.push_str("Server: Blitzkrieg\r\n");
-        res.push_str("\r\n");
-        let mut res = res.as_bytes().to_owned();
-        res.append(&mut self.body.unwrap());
-        res
     }
 }
